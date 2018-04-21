37b62b127fc0c66f72c3246caf0ffb56e63265e8
TechRunner2 <techrunner2.0@gmail.com>
2018-04-21 17:13:37 -0500

    Added contact form that just prints to consol log

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function send_email(){
+    var name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var message = document.getElementById('message');
+
+    console.log('Name = ', name.value);
+    console.log('Email = ', email.value);
+    console.log('Message = ', message.value);
+}
